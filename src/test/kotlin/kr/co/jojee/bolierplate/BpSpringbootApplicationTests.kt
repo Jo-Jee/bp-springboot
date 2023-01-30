41282534e396373b5e08b1bd387a5416febae754
Jo-Jee <capjjo@gmail.com>
2023-01-30 12:40:17 +0900

    init

---
+++ src/test/kotlin/kr/co/jojee/bolierplate/BpSpringbootApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.co.jojee.bolierplate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BpSpringbootApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
