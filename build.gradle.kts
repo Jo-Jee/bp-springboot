d630b0f2074a1adbc15420c63438ee149e19bf9e
Jo-Jee <capjjo@gmail.com>
2023-01-30 13:23:53 +0900

    swagger ui 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,10 +20,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
     runtimeOnly("com.h2database:h2")
